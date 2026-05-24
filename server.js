87f09dd6a9d9a50f2995851b6ef812e422dc984f
Matteo <teocarrera03@gmail.com>
2026-05-24 15:13:00 +0200

    fix porta e package-lock

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get('/api/aerei', async (req, res) => {
   }
 });
 
-app.listen(3001, () => {
-  console.log('✅ Server avviato su http://localhost:3001');
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {
+  console.log('✅ Server avviato su porta ' + PORT);
 });
